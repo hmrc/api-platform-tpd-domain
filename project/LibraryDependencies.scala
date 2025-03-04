f9b6b48a6e78ffb9b584b16c1a97d60e92e78467
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2025-03-04 08:25:34 +0000

    APIS-7497 PR bot actions

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -3,17 +3,15 @@ import sbt._
 object LibraryDependencies {
   lazy val commonDomainVersion = "0.17.0"
   
-  lazy val tpdDomainDeps = compileDependencies ++ testCommonDependencies.map(_ % "test") ++ testDependencies.map(_ % "test")
+  lazy val tpdDomainDeps = compileDependencies ++ testDependencies.map(_ % "test")
 
-  lazy val tpdTestDomainDeps = compileDependencies ++ testCommonDependencies
+  lazy val tpdTestDomainDeps = compileDependencies ++ testDependencies
   
   lazy val compileDependencies = Seq(
     "uk.gov.hmrc"             %% "api-platform-common-domain"     % commonDomainVersion
   )
 
-  lazy val testCommonDependencies = Seq (
+  lazy val testDependencies = Seq (
     "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures" % commonDomainVersion
   )
-
-  lazy val testDependencies = Seq[ModuleID]()
 }
